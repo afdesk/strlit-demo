11511a236bf49094b8dddea1bbe4d8aac93fad6c
afdesk <work@afdesk.com>
2023-03-24 17:49:23 +0600

    add aiu lite task

---
+++ aiu_lite.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+
+values = range(10)
+
+labels = ['all'] + list(map(str, values))
+
+selected = st.sidebar.multiselect(
+    'How to filter the values?',
+    labels,
+)
+
+if 'all' in selected:
+    st.write(np.array(values))
+else:
+    st.write(np.array(selected))
